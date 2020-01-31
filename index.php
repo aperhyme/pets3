238a626e97ea838fc17fa5a6cad8c1c20ed75d2e
Alex Grigorenko <agrigorenko@mail.greenriver.edu>
2020-01-31 13:32:57 -0500

    changed routes in index and edited from action

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ require("vendor/autoload.php");
 $f3 = Base::instance();
 $f3->set('colors', array('pink', 'green', 'blue'));
 
+//set debug level
+$f3->set('DEBUG', 3);
+
 // Define a default route
 $f3->route('GET /', function () {
     echo "<h1> My Pets <br></h1>";
@@ -20,12 +23,12 @@ $f3->route('GET /', function () {
     //echo $views->render("views/home.html");
 });
 
-$f3->route('GET /order', function() {
+$f3->route('GET|POST /order', function() {
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 
-$f3->route('POST /order2', function() {
+$f3->route('GET|POST /order2', function() {
 //    var_dump($_POST);
     $_SESSION['pets'] = $_POST['animalName'];
     $view = new Template();
